f66705954a2ae3ec9ff08218047945f8fbbbe327
samcyn <samsoniyanda@outlook.com>
2024-05-15 16:34:26 +0100

    feat(auth): set up firebase for authentication

--- tasks/frontEnd/src/main.ts
+++ tasks/frontEnd/src/main.ts
@@ -1,10 +1,19 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { VueFire, VueFireAuth } from 'vuefire';
+
+import App from '@/App.vue';
+import router from '@/router';
+import { firebaseApp } from './firebase';
 import './assets/main.css';
 
 const app = createApp(App);
 
 app.use(router);
-
+// set up firebase here...
+app.use(VueFire, {
+  firebaseApp,
+  modules: [
+    VueFireAuth(),
+  ],
+});
 app.mount('#app');
