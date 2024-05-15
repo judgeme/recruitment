04e25cac2cb70807c95c78cf1b64e0b9cb03e58d
samcyn <samsoniyanda@outlook.com>
2024-05-15 15:40:52 +0100

    style(linting): add configuration to eslint and prettier

--- tasks/frontEnd/.eslintrc.cjs
+++ tasks/frontEnd/.eslintrc.cjs
@@ -1,9 +1,9 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,15 +11,15 @@ module.exports = {
   ],
   overrides: [
     {
-      files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
+      files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:playwright/recommended']
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    semi: ['error', 'always']
   }
-}
+};
