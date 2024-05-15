c906b7a7c6313b428400711ab97b78a6f8e8b80b
samcyn <samsoniyanda@outlook.com>
2024-05-15 16:28:45 +0100

    style(tailwind): add more parameters and configuration to tailwind config file

--- tasks/frontEnd/src/App.vue
+++ tasks/frontEnd/src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-
+import AppHeader from '@/components/AppHeader/index.vue';
+import AppFooter from '@/components/AppFooter/index.vue';
 </script>
 
 <template>
-  <RouterView />
+  <section class="py-27 md:py-29">
+    <app-header />
+    <router-view />
+  </section>
+  <app-footer />
 </template>
--- tasks/frontEnd/tailwind.config.js
+++ tasks/frontEnd/tailwind.config.js
@@ -1,9 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#151124',
+        secondary: '#52F597',
+        danger: 'red',
+        'black-80': '#313131',
+        'black-70': '#080808',
+        'grey-40': '#929292',
+        'grey-60': '#e1e1e1',
+        'grey-80': '#999999',
+        'grey-light': '#f3f3f3',
+        'grey-deep': '#555555',
+        fairy: '#fafafa'
+      },
+      spacing: {
+        13: '52px',
+        15: '60px',
+        18: '72px',
+        25: '100px',
+        30: '120px',
+        27: '108px',
+        29: '116px',
+        50: '200px',
+        100: '400px'
+      },
+      fontSize: {
+        tiny: ['12px', '30px'],
+        small: ['10px', '25px'],
+        anchor: ['13px', '25px'],
+        input: ['13px', '18px'],
+        link: ['12px', '23px'],
+        'heading-3': ['24px', {
+          lineHeight: '36px',
+          letterSpacing: '2px',
+        }],
+      },
+      letterSpacing: {
+        compact: '1.8px',
+        mono: '1px',
+        double: '2px',
+        link: '1.2px'
+      },
+      fontFamily: {
+        inherit: 'inherit'
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
