238c7d4122d30e620381a4f011352a07deca3bf7
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-05-17 22:28:46 +0200

    Add `by_shop` scope to review

--- tasks/fullStack/app/models/review.rb
+++ tasks/fullStack/app/models/review.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Review < ApplicationRecord
   belongs_to :product
+
+  scope :by_shop, ->(shop) { joins(product: :shop).where(shops: { id: shop }) }
 end
--- tasks/fullStack/spec/models/review_spec.rb
+++ tasks/fullStack/spec/models/review_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe Review, type: :model do
     it { is_expected.to belong_to(:product) }
   end
 
-  # describe 'validations' do
-  #   it { is_expected.to validate_presence_of(:attribute) }
-  # end
+  describe '.by_shop' do
+    it 'returns only review by shop_id' do
+      shop = create(:shop)
+      product1 = build(:product, shop: shop)
+      review1 = create(:review, product: product1)
+      product2 = build(:product)
+      review2 = create(:review, product: product2)
+
+      expect(Review.by_shop(shop)).to include(review1)
+      expect(Review.by_shop(shop)).not_to include(review2)
+    end
+  end
 end
