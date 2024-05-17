2b3cac6d04d47485fe143d89ce1d8b8bda33d4c8
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-05-17 21:33:14 +0200

    Make system tests run headlessly (without opening a browser)

--- tasks/fullStack/spec/support/capybara.rb
+++ tasks/fullStack/spec/support/capybara.rb
@@ -3,8 +3,19 @@
 require 'capybara/rails'
 require 'capybara/rspec'
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[headless disable-gpu no-sandbox]
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.javascript_driver = :selenium_chrome
+Capybara.javascript_driver = :selenium_chrome_headless
+
+RSpec.configure do |config|
+  config.include(Capybara::DSL)
+  config.before(:each, type: :system) do
+    driven_by(:selenium_chrome_headless)
+  end
+end
