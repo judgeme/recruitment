452603ffdc77e4a0f625d065a22e89ff6bfa7506
Maya Angelica Hernandez <mayaah@berkeley.edu>
2024-05-19 19:30:48 +0100

    Make test description more accurate

--- tasks/fullStack/spec/requests/reviews_spec.rb
+++ tasks/fullStack/spec/requests/reviews_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Reviews", type: :request do
   describe "#create" do
-    it "can make a new review" do  
+    it "can return success" do
       get new_review_path
       expect(response).to be_successful
     end
