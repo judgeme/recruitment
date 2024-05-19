160335be8e0d14fa8b1bb2d865b8d34059c2cfb1
Jed Stanton <jstanton@cegid.com>
2024-05-19 13:52:19 +0100

    Optimize product query and add support for fetching without shop_id
    Initially users weren't able to view any products unless shop_id param was
    present. In this commit I've also optimised some of the existing queries as
    well as removed string injection in sql queries.

--- tasks/fullStack/app/controllers/reviews_controller.rb
+++ tasks/fullStack/app/controllers/reviews_controller.rb
@@ -3,16 +3,21 @@ class ReviewsController < ApplicationController
   DEFAULT_TAGS = ['default']
 
   def index
-    if params[:shop_id].present? && Shop.where("id = #{params[:shop_id]}").present?
-      params[:per_page] ||= 10
-      offset = params[:page].to_i * params[:per_page]
+    params[:per_page] ||= 10
+    params[:page] ||= 1
+    offset = (params[:page].to_i - 1) * params[:per_page].to_i
 
-      @data = []
-      products = Product.where("shop_id = #{params[:shop_id]}").sort_by(&:created_at)[offset..(offset + params[:per_page])]
-      products.each do |product|
-        reviews = product.reviews.sort_by(&:created_at)[offset..(offset + params[:per_page])]
-        @data << { product: product, reviews: reviews }
-      end
+    @data = []
+
+    products_query = Product.all
+    products_query = products_query.where(shop_id: params[:shop_id]) if params[:shop_id].present?
+    products_query = products_query.order(created_at: :asc)
+
+    products = products_query.includes(:reviews).limit(params[:per_page]).offset(offset)
+
+    products.each do |product|
+      reviews = product.reviews.order(created_at: :desc).limit(params[:per_page]).offset(offset)
+      @data << { product: product, reviews: reviews }
     end
   end
 
