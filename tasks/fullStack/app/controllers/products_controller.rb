b798b18b22030af002eee8a13616007eb802ee07
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-05-19 17:57:57 +0200

    Add Product page functionality with ransack and pagy WIP

--- tasks/fullStack/app/controllers/products_controller.rb
+++ tasks/fullStack/app/controllers/products_controller.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  def index
+    @q = Product.ransack(params[:q])
+    pagy, products = pagy(@q.result(distinct: true), items: 10)
+
+    render locals: {
+      shops: shops,
+      products: products,
+      pagy: pagy
+    }
+  end
+
+  private
+
+  def search_params
+    params.permit(
+      :format,
+      :commit,
+      q: %i[shop_id_eq]
+    )
+  end
+
+  def shops
+    @shops ||= Shop.all.order(name: :asc)
+  end
 end
