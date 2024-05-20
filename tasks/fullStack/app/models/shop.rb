fde7277156cd1de33b0b994254c0d8825d8681e7
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-05-20 23:01:03 +0100

    Add scope `Shop#with_reviews`

--- tasks/fullStack/app/models/shop.rb
+++ tasks/fullStack/app/models/shop.rb
@@ -1,4 +1,8 @@
 class Shop < ApplicationRecord
   has_many :products
   has_many :reviews, through: :products
+
+  scope :with_reviews, lambda {
+    joins(products: :reviews).distinct
+  }
 end
--- tasks/fullStack/spec/models/shop_spec.rb
+++ tasks/fullStack/spec/models/shop_spec.rb
@@ -8,7 +8,19 @@ RSpec.describe Shop, type: :model do
     it { is_expected.to have_many(:reviews).through(:products) }
   end
 
-  # describe 'validations' do
-  #   it { is_expected.to validate_presence_of(:attribute) }
-  # end
+  describe '.with_reviews' do
+    it 'returns only shops that have reviews' do
+      shop1 = create(:shop)
+      shop2 = create(:shop)
+      shop3 = create(:shop)
+
+      create(:product, :with_reviews, shop: shop1)
+      create(:product, :with_reviews, shop: shop3)
+
+      scope = Shop.with_reviews
+
+      expect(scope).to include(shop1, shop3)
+      expect(scope).not_to include(shop2)
+    end
+  end
 end
