52c87a3324c63c2362c5007876b25ec4a341783e
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-05-21 18:39:32 +0100

    Add few Review scopes

--- tasks/fullStack/app/models/review.rb
+++ tasks/fullStack/app/models/review.rb
@@ -4,4 +4,13 @@ class Review < ApplicationRecord
   belongs_to :product
 
   scope :by_shop, ->(shop) { joins(product: :shop).where(shops: { id: shop }) }
+
+  scope :before_date, lambda { |date|
+    table = Review.arel_table
+    condition = table[:created_at].lteq(date.end_of_month)
+
+    where(condition)
+  }
+
+  scope :by_product, ->(product) { where(product_id: product.id) }
 end
--- tasks/fullStack/spec/models/review_spec.rb
+++ tasks/fullStack/spec/models/review_spec.rb
@@ -19,4 +19,34 @@ RSpec.describe Review, type: :model do
       expect(Review.by_shop(shop)).not_to include(review2)
     end
   end
+
+  describe '.before_date' do
+    before { travel_to(Time.zone.local(2023, 10, 17, 7, 24)) }
+    after { travel_back }
+
+    it 'returns only reviews before specified date month' do
+      review1 = create(:review, created_at: 1.month.ago)
+      review2 = create(:review, created_at: 1.month.ago + 2.days)
+      review3 = create(:review, created_at: 1.day.ago)
+
+      scope = Review.before_date(1.month.ago)
+
+      expect(scope).to include(review1, review2)
+      expect(scope).not_to include(review3)
+    end
+  end
+
+  describe '.by_product' do
+    it 'returns reviews by product' do
+      product = create(:product)
+      review1 = create(:review, product: product)
+      review2 = create(:review)
+      review3 = create(:review)
+
+      scope = Review.by_product(product)
+
+      expect(scope).to include(review1)
+      expect(scope).not_to include(review2, review3)
+    end
+  end
 end
